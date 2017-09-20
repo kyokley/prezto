3f6b7e26865a564dd6377d84d4949f1040bfcab2
Kevin Yokley <kyokley2@gmail.com>
2017-09-20 17:25:49 -0500

    Removing extra cautious rm alias

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -65,7 +65,7 @@ alias mvi="${aliases[mv]:-mv} -i"
 alias cpi="${aliases[cp]:-cp} -i"
 alias lni="${aliases[ln]:-ln} -i"
 if zstyle -T ':prezto:module:utility' safe-ops; then
-  alias rm='rmi'
+  #alias rm='rmi'
   alias mv='mvi'
   alias cp='cpi'
   alias ln='lni'
